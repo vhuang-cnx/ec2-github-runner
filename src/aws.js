c2b678ef099b1db3eea1110b81cdc31507204f85
Vance Huang <vance.huang@concentrix.com>
2023-05-05 15:38:58 -0500

    simple

--- src/aws.js
+++ src/aws.js
@@ -30,45 +30,67 @@ function buildUserDataScript(githubRegistrationToken, label, noRunner = false) {
   }
 
   if (config.input.ec2Os === 'windows') {
-    var userData;
-    if (config.input.runnerHomeDir) {
-      userData = [
-        '<powershell>',
-        `cd "${config.input.runnerHomeDir}"`
-      ]
-    } else {
-      // If runner home directory is specified, we expect the actions-runner software (and dependencies)
-      // to be pre-installed in the AMI, so we simply cd into that directory and then start the runner
-      userData = [
-        '<powershell>',
-        'mkdir actions-runner; cd actions-runner',
-        `Invoke-WebRequest -Uri https://github.com/actions/runner/releases/download/v${runnerVersion}/actions-runner-win-x64-${runnerVersion}.zip -OutFile actions-runner-win-x64-${runnerVersion}.zip`,
-        `Add-Type -AssemblyName System.IO.Compression.FileSystem ; [System.IO.Compression.ZipFile]::ExtractToDirectory("$PWD/actions-runner-win-x64-${runnerVersion}.zip", "$PWD")`
-      ]
-    }          
-
     // We cannot attach ENI first because it will break 169.254.169.254 access
     if (config.input.eniId) {
-      // If here, we need to add ENI attachment
-      return userData.concat( [
-        'whoami > c:\\Users\\test.txt',
-        `$instanceId = (invoke-webrequest http://169.254.169.254/latest/meta-data/instance-id -UseBasicParsing).content`,
-        `$result = aws ec2 attach-network-interface --network-interface-id ${config.input.eniId} --instance-id $instanceId --device-index 1 | ConvertFrom-Json`,
-        'echo $result.AttachmentId >> c:\\Users\\test.txt',
-        '[Environment]::SetEnvironmentVariable("LIC_ATTACHMENT_ID",$result.AttachmentId, "User")',
-        '[Environment]::SetEnvironmentVariable("LIC_ATTACHMENT_ID",$result.AttachmentId, "Machine")',
-        `./config.cmd --url https://github.com/${config.githubContext.owner}/${config.githubContext.repo} --token ${githubRegistrationToken} --labels ${label} --unattended`,
-        './run.cmd',
-        '</powershell>',
-        '<persist>false</persist>'
-      ] )
+      if (config.input.runnerHomeDir) {
+        return [
+          '<powershell>',
+          'whoami > c:\\Users\\test.txt',
+          `$instanceId = (invoke-webrequest http://169.254.169.254/latest/meta-data/instance-id -UseBasicParsing).content`,
+          `$result = aws ec2 attach-network-interface --network-interface-id ${config.input.eniId} --instance-id $instanceId --device-index 1 | ConvertFrom-Json`,
+          'echo $result.AttachmentId >> c:\\Users\\test.txt',
+          '[Environment]::SetEnvironmentVariable("LIC_ATTACHMENT_ID",$result.AttachmentId, "User")',
+          '[Environment]::SetEnvironmentVariable("LIC_ATTACHMENT_ID",$result.AttachmentId, "Machine")',
+          `cd "${config.input.runnerHomeDir}"`,
+          `./config.cmd --url https://github.com/${config.githubContext.owner}/${config.githubContext.repo} --token ${githubRegistrationToken} --labels ${label} --unattended`,
+          './run.cmd',
+          '</powershell>',
+          '<persist>false</persist>'
+          ]
+      } else {
+        // If runner home directory is specified, we expect the actions-runner software (and dependencies)
+        // to be pre-installed in the AMI, so we simply cd into that directory and then start the runner
+        return [
+          '<powershell>',
+          'mkdir actions-runner; cd actions-runner',
+          `Invoke-WebRequest -Uri https://github.com/actions/runner/releases/download/v${runnerVersion}/actions-runner-win-x64-${runnerVersion}.zip -OutFile actions-runner-win-x64-${runnerVersion}.zip`,
+          `Add-Type -AssemblyName System.IO.Compression.FileSystem ; [System.IO.Compression.ZipFile]::ExtractToDirectory("$PWD/actions-runner-win-x64-${runnerVersion}.zip", "$PWD")`,
+          'whoami > c:\\Users\\test.txt',
+          `$instanceId = (invoke-webrequest http://169.254.169.254/latest/meta-data/instance-id -UseBasicParsing).content`,
+          `$result = aws ec2 attach-network-interface --network-interface-id ${config.input.eniId} --instance-id $instanceId --device-index 1 | ConvertFrom-Json`,
+          'echo $result.AttachmentId >> c:\\Users\\test.txt',
+          '[Environment]::SetEnvironmentVariable("LIC_ATTACHMENT_ID",$result.AttachmentId, "User")',
+          '[Environment]::SetEnvironmentVariable("LIC_ATTACHMENT_ID",$result.AttachmentId, "Machine")',
+          `./config.cmd --url https://github.com/${config.githubContext.owner}/${config.githubContext.repo} --token ${githubRegistrationToken} --labels ${label} --unattended`,
+          './run.cmd',
+          '</powershell>',
+          '<persist>false</persist>'
+        ]
+      } 
     } else {
-      return userData.concat( [
-        `./config.cmd --url https://github.com/${config.githubContext.owner}/${config.githubContext.repo} --token ${githubRegistrationToken} --labels ${label} --unattended`,
-        './run.cmd',
-        '</powershell>',
-        '<persist>false</persist>'
-      ] )
+      if (config.input.runnerHomeDir) {
+        return [
+          '<powershell>',
+          `cd "${config.input.runnerHomeDir}"`,
+          `./config.cmd --url https://github.com/${config.githubContext.owner}/${config.githubContext.repo} --token ${githubRegistrationToken} --labels ${label} --unattended`,
+          './run.cmd',
+          '</powershell>',
+          '<persist>false</persist>'
+          ]
+      } else {
+        // If runner home directory is specified, we expect the actions-runner software (and dependencies)
+        // to be pre-installed in the AMI, so we simply cd into that directory and then start the runner
+        return [
+          '<powershell>',
+          'mkdir actions-runner; cd actions-runner',
+          `Invoke-WebRequest -Uri https://github.com/actions/runner/releases/download/v${runnerVersion}/actions-runner-win-x64-${runnerVersion}.zip -OutFile actions-runner-win-x64-${runnerVersion}.zip`,
+          `Add-Type -AssemblyName System.IO.Compression.FileSystem ; [System.IO.Compression.ZipFile]::ExtractToDirectory("$PWD/actions-runner-win-x64-${runnerVersion}.zip", "$PWD")`,
+          `./config.cmd --url https://github.com/${config.githubContext.owner}/${config.githubContext.repo} --token ${githubRegistrationToken} --labels ${label} --unattended`,
+          './run.cmd',
+          '</powershell>',
+          '<persist>false</persist>'
+        ]
+      } 
     }
   } else if (config.input.ec2Os === 'linux') {
     if (config.input.runnerHomeDir) {
