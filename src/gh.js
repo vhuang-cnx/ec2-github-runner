2f65e6f45563d5fd533d14cd56fdabcf3d24d8a4
Vance Huang <107148187+vhuang-cnx@users.noreply.github.com>
2025-03-13 00:09:53 -0500

    Update gh.js

--- src/gh.js
+++ src/gh.js
@@ -52,7 +52,7 @@ async function removeRunner() {
 }
 
 async function waitForRunnerRegistered(label) {
-  const timeoutMinutes = 10;
+  const timeoutMinutes = 20;
   const retryIntervalSeconds = 10;
   const quietPeriodSeconds = 30;
   let waitSeconds = 0;
